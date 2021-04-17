d257ecb3bd4ec247e424ff8810ca38046dd65b64
Ginokun <meghinasso@gmail.com>
2021-04-17 10:44:32 -0300

    Se agregan los datos del alumno MEGHINASSO, Matias.

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s MATIAS_MEGHINASSO = {
+    .apellidos = "MEGHINASSO",
+    .nombres = "Matias Gino",
+    .documento = "36.806.968",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &MATIAS_MEGHINASSO,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
