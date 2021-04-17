9e1c23e4d26f73d635493dc1dabd5de02e41af2d
Alejandro <alejandrorougier@gmail.com>
2021-04-17 10:37:30 -0300

    Se agregan los datos del alumno ROUGIER, Alejandro

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s ALEJANDRO_ROUGIER = {
+    .apellidos = "ROUGIER",
+    .nombres = "Omar Alejandro",
+    .documento = "22.661.059",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &ALEJANDRO_ROUGIER,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
