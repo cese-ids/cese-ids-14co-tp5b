e710ddfef17a980d445981a02c12fec71b69334e
Jr130797 <ivanvarmar-13@hotmail.com>
2021-04-17 09:56:30 -0400

    Se agregan los datos del alumno VARGAS MARZA, Ivan

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s IVAN_VARGAS_MARZA = {
+    .apellidos = "VARGAS MARZA",
+    .nombres = "Ivan",
+    .documento = "9.394.693",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &IVAN_VARGAS_MARZA,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
