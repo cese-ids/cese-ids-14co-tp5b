b2842fcd37281e1d4d30202429f33b459fc1f7cd
DMC1988 <damian.m.caputo@gmail.com>
2021-04-17 13:32:58 -0300

    Se agregan los datos del alumno CAPUTO, Damian

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s DAMIAN_CAPUTO = {
+    .apellidos = "CAPUTO",
+    .nombres = "Damian",
+    .documento = "34.137.919",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &DAMIAN_CAPUTO,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
